0b1f270e6548018ea9aa33d3559922b3af4593f2
Tim Kofoed <tk@edmk.dk>
2016-01-21 13:38:17 +0100

    Lav objekter og giv point og mist liv

---
+++ MatchShooter/Assets/ObjectController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectController : MonoBehaviour {
+
+    public float hastighed;
+    public GameObject objekt;
+    private SceneController controller;
+    public ObjektType minType;
+    private Material mitMaterial;
+
+    void FixedUpdate()
+    {
+        objekt.transform.position = new Vector3(objekt.transform.position.x,
+                                                objekt.transform.position.y - hastighed,
+                                                objekt.transform.position.z);
+    }
+
+    void HarRamtBunden()
+    {
+        controller.MistLiv();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        HarRamtBunden();
+        Destroy(this.gameObject);
+    }
+
+    void OnMouseDown()
+    {
+        if(controller.HarTrykketPaaObjekt(minType) )
+        {
+            Destroy(this.gameObject);
+        }
+        
+    }
+
+    // Use this for initialization
+    void Start () {
+        controller = GameObject.FindWithTag("sceneController").GetComponent<SceneController>();
+        int valgtType = Random.Range(0,3);
+        minType = (ObjektType)valgtType;
+        mitMaterial = new Material( this.gameObject.GetComponent<Renderer>().material );
+
+        if (minType == ObjektType.objekt1)
+            mitMaterial.color = Color.blue;
+        else if (minType == ObjektType.objekt2)
+            mitMaterial.color = Color.red;
+        else if (minType == ObjektType.objekt3)
+            mitMaterial.color = Color.black;
+        else
+            Debug.LogError("Jeg kender ikke typen " + minType);
+
+        this.gameObject.GetComponent<Renderer>().material = mitMaterial;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ MatchShooter/Assets/SceneController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum ObjektType
+{
+    objekt1,
+    objekt2,
+    objekt3
+}
+
+public class SceneController : MonoBehaviour {
+    public GameObject fallendeObjektPrefab;
+    public Transform startPosition;
+    public int liv = 5;
+    public int score = 0;
+    public ObjektType valgteObjektType;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine( LavObjektInterval() );
+	}
+	
+    IEnumerator LavObjektInterval()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds( Random.Range(1.0f, 2.5f) );
+            GameObject.Instantiate(fallendeObjektPrefab, startPosition.position, startPosition.rotation);
+        }
+        
+    }
+
+    public void MistLiv()
+    {
+        liv -= 1;
+    }
+
+    public bool HarTrykketPaaObjekt(ObjektType objektViTrykkedePaa)
+    {
+        if (objektViTrykkedePaa == valgteObjektType)
+        {
+            score += 5;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
